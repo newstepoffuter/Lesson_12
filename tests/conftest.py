583858e9369f8a02a497163ddd2fbbb5fb421bb6
Nikita Semenov <n.semenov@exportcenter.ru>
2024-12-26 14:03:20 +0300

    Lesson 12

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,45 @@
 import pytest
+import os
 from selene import browser
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from utils import attach
+from dotenv import load_dotenv
+
+DEFAULT_BROWSER_NAME = "chrome"
+DEFAULT_BROWSER_VERSION = "126.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name")
+    parser.addoption("--browser_version")
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope="function")
-def browser_settings():
+def browser_settings(request):
+    browser_name = request.config.getoption('browser_name') or DEFAULT_BROWSER_NAME
+    browser_version = request.config.getoption('browser_version') or DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "126.0",
+        "browserName": browser_name,
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
-    options.page_load_strategy = 'eager'
+
     browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
     browser.config.driver.maximize_window()
