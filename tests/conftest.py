cde1cc051a1ab4682973a18d3f6c480b1d14db52
Nikita Semenov <n.semenov@exportcenter.ru>
2025-01-08 23:08:10 +0300

    Lesson 12 Fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,9 +37,9 @@ def browser_settings(request):
     options.capabilities.update(selenoid_capabilities)
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
-    selenoid_host = os.getenv('SELENOID_HOST')
+    selenoid_host = os.getenv("SELENOID_HOST")
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{selenoid_host}/wd/hub",
         options=options)
 
     browser.config.driver = driver
